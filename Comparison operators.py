c2cc78321f54c1f7e1a212ad14a0478d07792dc7
Kritika Pargain <68428226+kritikapargain@users.noreply.github.com>
2021-11-21 22:34:59 +0530

    Add files via upload

---
+++ Comparison operators.py
@@ -0,0 +1,23 @@
+x = 5
+y = 7
+
+
+print('x > y is',x>y)
+
+
+print('x < y is',x<y)
+
+
+print('x == y is',x==y)
+
+print('x >= y is',x>=y)
+
+
+print('x <= y is',x<=y)
+
+print('x and y is ',x and y)
+
+print('x or y is ',x or y)
+
+print('not x is ',not x)
+
